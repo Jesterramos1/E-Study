c82bf01a7ec654800fb8bbbc6d21126fcd1f5fd1
EuniceeMMM <48358341+EuniceeMMM@users.noreply.github.com>
2022-09-29 13:51:34 +0800

    Search bar
    When user type all elements will hide then the result will appear

--- fetch.php
+++ fetch.php
@@ -3,14 +3,16 @@
 $con = mysqli_connect("localhost", "root", "", "estudy_db");
 
 $output = '';
-$sql = "SELECT * FROM storage WHERE title LIKE '{$_POST['search']}%'";
+$sql = "SELECT * FROM storage WHERE title LIKE '{$_POST['search']}%' OR ";
 $result = mysqli_query($con, $sql);
 
 if(mysqli_num_rows($result) > 0)
+
 {
-    $output .= '<h4 align="center"> Search Result</h4>';
+   
     $output .='<div class="table-responsive">
                     <table class="table table bordered">
+                
                         <tr>
                             <th>Title</th>
                             <th>Department</th>
@@ -32,8 +34,10 @@ if(mysqli_num_rows($result) > 0)
                     </tr>';
 
     }
-
+    
     echo $output;
+
+
 
 }else{
     echo "Data not found";
