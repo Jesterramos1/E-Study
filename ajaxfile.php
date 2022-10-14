da525d7a9c4138a88c11aff92fe8e0bce4eedb7e
Jesterramos1 <114465045+Jesterramos1@users.noreply.github.com>
2022-10-14 21:27:36 +0800

    Update ajaxfile.php

--- ajaxfile.php
+++ ajaxfile.php
@@ -18,25 +18,36 @@ while( $row = mysqli_fetch_array($result) ){
         </div>  
     </div>
     <div class="col-md-5 col-sm-12" >
-        <label ></label>
-
-
-
-        <table border='0' width='100%'>
-        <tr>
-            <td>
-            <p>Title : <?php echo $row['title']; ?></p>
-            <p>Department : <?php echo $row['department']; ?></p>
-            <p>Date Publish : <?php echo $row['date_publish']; ?></p>
-            <p>Researchers : <?php echo $row['researchers']; ?></p>
-            <p>Location : <?php echo $row['location']; ?></p>
-            </td>
-        </tr>
-        </table>
-        <div class="modal-footer">
-            <a class="btn btn-primary" href="scheduling.php" role="button">Book Visit</a>
+    <div class="card">
+        <div class="card-header">
+            Details
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                Title :<br><?php echo $row['title']; ?>
+            </li>
+            <li class="list-group-item">
+                Department :<br><?php echo $row['department']; ?>
+            </li>
+            <li class="list-group-item">
+                Date Published :<br><?php echo $row['date_publish']; ?>
+            </li>
+            <li class="list-group-item">
+                Researchers :<br><?php echo $row['researchers']; ?>
+            </li>
+            <li class="list-group-item">
+                Location :<br><?php echo $row['location']; ?>
+            </li>
+            
+        </ul>
+        <div class="card-footer">
+            <div class="modal-footer">
+                <a class="btn btn-primary" href="scheduling.php" role="button">Book Visit</a>
+            </div>
         </div>
     </div>
+    </div>
+</div>    
     
  
 <?php } ?>
