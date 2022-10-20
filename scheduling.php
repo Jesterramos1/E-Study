0149440c34f4532a66880d1ca4faea44d502595b
EuniceeMMM <48358341+EuniceeMMM@users.noreply.github.com>
2022-10-20 11:35:33 +0800

    updated scheduling

--- scheduling.php
+++ scheduling.php
@@ -40,7 +40,7 @@
             <h5 class="card-header" >LIBRARY HOUSE RULES</h5>
               <div class="card-body">
                 <div class="row" >
-                  <img src="images/rule.png" class="img-fluid" alt="rules" style="height:220px;">
+                  <img src="images/rule.png" class="img-fluid" alt="rules" style="height:276px; object-fit:cover;">
                 </div>
               </div>
           </div>
@@ -115,100 +115,105 @@
       </div>
     </div>
   </div>
+
+  <!--Visitation-->
   <div class="col-lg-7 col-sm-12" id="forms">
-    <div class="card text-center" >
-      <h5 class="card-header" >
-        BOOK VISITATION
-      </h5>
-      <div class="card-body">
-        <form action="insert.php" method="POST" name="info">
+    <div class="card" >
+      <h5 class="card-header" style="text-align:center;">BOOK VISITATION</h5>
+        <div class="card-body">
+          <form action="insert.php" method="POST" name="info">
 
-        <!--INPUT FOR USER INFORMATION-->
-        <div class="mb-3">
-        <div class="input-group">
+          <!--INPUT FOR USER INFORMATION-->
+          <div class="mb-3">
+          <div class="input-group">
 
-        <span class="input-group-text" style="background:#1C5090; color:white;"><b>Full Name:</b></span>
-        <input type="text" aria-label="First name" name="fname" class="form-control" placeholder="Juan" required>
-        <input type="text" aria-label="Last name" name="lname" class="form-control" placeholder="Dela Cruz" required>
+          <span class="input-group-text" style="background:#1C5090; color:white;"><b>Full Name:</b></span>
+          <input type="text" aria-label="First name" name="fname" class="form-control" placeholder="Juan" required>
+          <input type="text" aria-label="Last name" name="lname" class="form-control" placeholder="Dela Cruz" required>
+
+          </div>
+          <span id="nameError"></span>
+          </div>
+
+          <div class="row g-2 mb-3">
+            <div class="col-md">
+                <label for="studNum" class="form-label" ><b>Student Number:</b></label>
+                <input class="form-control" type="text"  maxlength="11" name ="studNum" id="studNum" placeholder="2019-123456" required>
+                <span id="studNumError"></span>
+            </div>
+            <div class="col-md">
+              <label for="course" class="form-label" ><b>Course:</b></label>
+              <input class="form-control" type="text"  name="course" id="course" placeholder="BSIT/BSINFOTECH" required>
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="barcode" class="form-label" ><b>Thesis Barcode:</b></label>
+            <input class="form-control" type="text"  name="barcode" id="barcode" placeholder="TLA125" required>
+          </div>
+
+          <div class="mb-3">
+          <label for="email" class="form-label" ><b>Email:</b></label>
+          <input class="form-control" type="email"  name="email" id="email" placeholder="delacruzjuan123@gmail.com" required>
+          <span id="studEmailError"></span>
+          </div>
+
+          <div class="mb-3">
+          <label for="contact" class="form-label" ><b>Contact Number:</b></label>
+          <input class="form-control" type="int"  name="contact" maxlength="11" id="contact" placeholder="09234567899" required>
+          </div>
+
+          <span><b>Location:</b></span>  
+            <div class=" form-floating mb-3">          
+                <select class="form-select" name ="library" id="library" required>
+                  <option selected value="ceat">CEAT Library - Multi-purpose Building / Third (3rd) Floor</option>
+                  <option value="cbet">CBET Library - SNAGAH Building / Second (2nd) Floor </option>
+                  <option value="cas">CAS Library - MAB Building / Second (2nd) Floor </option>
+                  <option value="ced">CED Library - SNAGAH Building / Second (2nd) Floor </option>
+                  <option value="ipe">IPE Library - MAB Building / Fifth (5th) Floor</option>
+                  <option value="gs">GS Library - RND Building / Third (3rd) Floor</option>
+                </select>
+                  <label for="library">Select Library:</label>
+            </div>
+
+          <!--DATE SELECTION-->
+          <div class="cal">
+          <label for="date" class="form-label" ><b>Select Date:</b></label> 
+          <input class="form-control" type="text" id="Calendar" name="calendar" placeholder="Choose Date" required>
+          </div>
+
+          <!--TIME SELECTION-->
+          <br>
+          <span><b>Preffered Time:</b></span> 
+            <div class="form-floating mb-3">
+                <select class="form-select" name="time" id="time" required>
+                  <option selected value="8:00 AM">08:00 AM</option>
+                  <option value="8:30 AM">08:30 AM</option>
+                  <option value="9:00 AM">09:00 AM</option>
+                  <option value="9:30 AM">09:30 AM</option>
+                  <option value="10:00 AM">10:00 AM</option>
+                  <option value="10:30 AM">10:30 AM</option>
+                  <option value="11:00 AM">11:00 AM</option>
+                  <option value="11:30 AM">11:30 AM</option>
+                  <option value="12:00 AM">12:00 PM</option>
+                  <option value="12:30 PM">12:30 PM</option>
+                  <option value="1:00 PM">01:00 PM</option>
+                  <option value="1:30 PM">01:30 PM</option>
+                  <option value="2:00 PM">02:00 PM</option>
+                  <option value="2:30 PM">02:30 PM</option>
+                  <option value="3:00 PM">03:00 PM</option>
+                </select>
+                  <label for="time">Select Time:</label>
+            </div>
+
+          <br>
+
+          <button name = "submit" type="submit" class="btn btn-primary" style="width:100%; background:#1C5090; text-transform:uppercase; font-size:16px; font-weight:bold;">Book Appointment</button>
+
 
         </div>
-        <span id="nameError"></span>
-        </div>
-
-        <div class="mb-3">
-        <label for="studNum" class="form-label" style="float: left;"><b>Student Number:</b></label>
-        <input class="form-control" type="text"  maxlength="11" name ="studNum" id="studNum" placeholder="2019-123456" required>
-        <span id="studNumError"></span>
-        </div>
-
-        <div class="mb-3">
-        <label for="course" class="form-label" style="float: left;"><b>Course:</b></label>
-        <input class="form-control" type="text"  name="course" id="course" placeholder="BSIT/BSINFOTECH" required>
-        </div>
-
-        <div class="mb-3">
-        <label for="email" class="form-label" style="float: left;"><b>Email:</b></label>
-        <input class="form-control" type="email"  name="email" id="email" placeholder="delacruzjuan123@gmail.com" required>
-        <span id="studEmailError"></span>
-        </div>
-
-        <div class="mb-3">
-        <label for="contact" class="form-label" style="float: left;"><b>Contact Number:</b></label>
-        <input class="form-control" type="int"  name="contact" maxlength="11" id="contact" placeholder="09234567899" required>
-        </div>
-
-        <div class="mb-3">
-        <label for="contact" class="form-label" style="float: left;"><b>Location:</b></label>  
-        <select class="form-select" name = "library" id="library" required>
-        <option selected value="">Select Library</option>
-        <option value="ceat">CEAT Library - Multi-purpose Building / Third (3rd) Floor</option>
-        <option value="cbet">CBET Library - SNAGAH Building / Second (2nd) Floor </option>
-        <option value="cas">CAS Library - MAB Building / Second (2nd) Floor </option>
-        <option value="ced">CED Library - SNAGAH Building / Second (2nd) Floor </option>
-        <option value="ipe">IPE Library - MAB Building / Fifth (5th) Floor</option>
-        <option value="gs">GS Library - RND Building / Third (3rd) Floor</option>
-        </select>
-        </div>
-
-        <!--DATE SELECTION-->
-        <div class="cal">
-        <label for="date" class="form-label" style="float: left;"><b>Select Date:</b></label> 
-        <input class="form-control" type="text" id="Calendar" name="calendar" placeholder="Choose Date" required>
-        </div>
-
-        <!--TIME SELECTION-->
-        <br>
-        <div id="clock">
-        <label for="time" class="form-label" style="float: left;"><b>Select Time:</b></label> 
-        <select class="form-select" name="time" id="time" required>
-        <option selected value="">Select Time</option>
-        <option value="8:00 AM">08:00 AM</option>
-        <option value="8:30 AM">08:30 AM</option>
-        <option value="9:00 AM">09:00 AM</option>
-        <option value="9:30 AM">09:30 AM</option>
-        <option value="10:00 AM">10:00 AM</option>
-        <option value="10:30 AM">10:30 AM</option>
-        <option value="11:00 AM">11:00 AM</option>
-        <option value="11:30 AM">11:30 AM</option>
-        <option value="12:00 AM">12:00 PM</option>
-        <option value="12:30 PM">12:30 PM</option>
-        <option value="1:00 PM">01:00 PM</option>
-        <option value="1:30 PM">01:30 PM</option>
-        <option value="2:00 PM">02:00 PM</option>
-        <option value="2:30 PM">02:30 PM</option>
-        <option value="3:00 PM">03:00 PM</option>
-        </select>
-        </div>
-
-        <br>
-
-        <button name = "submit" type="submit" class="btn btn-primary" style="width:100%; background:#1C5090; text-transform:uppercase; font-size:16px; font-weight:bold;">Book Appointment</button>
-
-
-      </div>
 
         <div class="card-footer text-muted">
-
           <div class="alert alert-primary d-flex align-items-center" role="alert">
             <i class="bi bi-info-square-fill" style="margin-right:1%;"></i>
           <div>
@@ -216,8 +221,7 @@
           </div>
         </div>
       </div>
-
-    </form>
+        </form>
     </div>
   </div>
 </div>
@@ -225,7 +229,6 @@
 
 
 <?php
-
   //CONNECTION/CREATION OF TABLE
   require 'dbtable_creation.php';
 ?>
