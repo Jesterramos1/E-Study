58867de33d65c13c200088575662dbb5574ce6f0
Jesterramos1 <114465045+Jesterramos1@users.noreply.github.com>
2022-10-20 09:42:54 +0800

    Update scheduling.php

--- scheduling.php
+++ scheduling.php
@@ -1,63 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!--BOOTSTRAP LINK-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" integrity="sha384-xeJqLiuOvjUBq3iGOjvSQSIlwrpqjSHXpduPd6rQpuiM3f5/ijby8pCsnbu5S81n" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<!--JQUERY AND CSS OF CALENDAR-->
-<link rel="stylesheet" href="calendar.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>	
-
-<!--JQUERY DATE PICKER-->
-<script>
-	$(document).ready(function(){
-$('#Calendar').datepicker({
-beforeShowDay: $.datepicker.noWeekends,
-minDate: 0
-});
-  
-    $('#fname, #lname, #studNum, #email').keyup(function(){
-    
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    var studNum = $('#studNum').val();
-    var email = $('#email').val();
-    $.ajax({
-      url: "Check.php",
-      method: "POST",
-      data:{fname:fname, lname:lname},
-      success: function(data){
-        $('#nameError').html(data);
-
-        
-        $.ajax({
-          url: "Check.php",
-          method: "POST",
-          data:{studNum:studNum},
-          success: function(data){
-            $('#studNumError').html(data);
-
-            $.ajax({
-              url: "Check.php",
-              method: "POST",
-              data:{email:email},
-              success: function(data){
-                $('#studEmailError').html(data);
-              }
-            });
-          }
-        });
-      }
-    });
-
-  }); 
-});
 </script>
 
 <title>Schedule</title>
@@ -83,96 +29,90 @@ minDate: 0
 
 <body>
 <!--HEADER-->
-<?php
+<?php require 'header.php'; ?>
 
-  require 'header.php'; 
+<!--Safety Reminder Form-->
+<div class="row" id="main">
+  <div class="col-lg-5 col-sm-12" id="forms">
+    <div class="row">
+      <div class="col-12">
+        <div class="card text-center" >
+            <h5 class="card-header" >LIBRARY HOUSE RULES</h5>
+              <div class="card-body">
+                <div class="row" >
+                  <img src="images/rule.png" class="img-fluid" alt="rules" style="height:220px;">
+                </div>
+              </div>
+          </div>
+      </div>
 
-  ?>
 
-<!--FORM-->
-<br><br>
-<div class="card text-center" style="width: 45%; margin-left: auto; margin-right: auto;">
-  <h5 class="card-header" style="font-family: Helvetica, sans-serif; font-weight: bold; background:#1C5090; color:white;">
-    SCHEDULE YOUR VISIT
-  </h5>
-  <div class="card-body">
-    <form action="insert.php" method="POST" name="info">
+      <div class="col-12">
+      <div class="card text-center" >
+        <h5 class="card-header" >SAFETY REMINDERS</h5>
+            <div class="card-body">
 
-    <!--INPUT FOR USER INFORMATION-->
-    <div class="mb-3">
-    <div class="input-group">
-    
-    <span class="input-group-text" style="background:#1C5090; color:white;"><b>Full Name:</b></span>
-    <input type="text" aria-label="First name" name="fname" id="fname" class="form-control" placeholder="Juan" required>
-    <input type="text" aria-label="Last name" name="lname" id="lname" class="form-control" placeholder="Dela Cruz" required>
+              <div class="row" >
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/disinfect.jfif" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">The library is sanitized regularly.</p>
+                          </div>
+                        </div>
+                </div>
 
-    </div>
-    <span id="nameError"></span>
-    </div>
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/vaccine.jpg" style="border-style:solid; border-color:#c7c0f3;" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">All employees are fully vaccinated.</p>
+                          </div>
+                        </div>
+                </div>
 
-    <div class="mb-3">
-    <label for="studNum" class="form-label"  pattern="/^[0-9]{4}\-[0-9]$/" style="float: left;"><b>Student Number:</b></label>
-    <input class="form-control" type="text" name ="studNum" id="studNum" placeholder="2019-123456" required maxlength="11">
-    <span id="studNumError"></span>
-    </div>
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/sanitation.jfif" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">Sanitation stations are available upon entry.</p>
+                          </div>
+                        </div>
+                </div>
+              </div>
 
-    <div class="mb-3">
-    <label for="course" class="form-label" style="float: left;"><b>Course:</b></label>
-    <input class="form-control" type="text"  name="course" id="course" placeholder="BSIT/BSINFOTECH" required>
-    </div>
 
-    <div class="mb-3">
-    <label for="email" class="form-label" pattern="/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/." style="float: left;"><b>Email:</b></label>
-    <input class="form-control" type="email"  name="email" id="email" placeholder="delacruzjuan123@gmail.com" required>
-    <span id="studEmailError"></span>
-    </div>
+              <div class="row" >
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/mask.jfif" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">Wear a mask at all times.</p>
+                          </div>
+                        </div>
+                </div>
 
-    <div class="mb-3">
-    <label for="contact" class="form-label" style="float: left;"><b>Contact Number:</b></label>
-    <input class="form-control" type="int"  name="contact" maxlength="11" id="contact" placeholder="09234567899" required>
-    </div>
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/shoerug.jpg" style="border-style:solid; border-color:#5bacee;" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">Wear a shoe rug inside the premises.</p>
+                          </div>
+                        </div>
+                </div>
 
-    <div class="mb-3">
-    <label for="contact" class="form-label" style="float: left;"><b>Location:</b></label>  
-    <select class="form-select" name = "library" id="library" required>
-    <option selected value="">Select Library</option>
-    <option value="ceat">CEAT Library - Multi-purpose Building / Third (3rd) Floor</option>
-    <option value="cbet">CBET Library - SNAGAH Building / Second (2nd) Floor </option>
-    <option value="cas">CAS Library - MAB Building / Second (2nd) Floor </option>
-    <option value="ced">CED Library - SNAGAH Building / Second (2nd) Floor </option>
-    <option value="ipe">IPE Library - MAB Building / Fifth (5th) Floor</option>
-    <option value="gs">GS Library - RND Building / Third (3rd) Floor</option>
-    </select>
-    </div>
-
-    <!--DATE SELECTION-->
-    <div class="cal">
-    <label for="date" class="form-label" style="float: left;"><b>Select Date:</b></label> 
-    <input class="form-control" type="text" id="Calendar" name="calendar" placeholder="Choose Date" required>
-    </div>
-
-    <!--TIME SELECTION-->
-    <br>
-    <div id="clock">
-    <label for="time" class="form-label" style="float: left;"><b>Select Time:</b></label> 
-    <select class="form-select" name="time" id="time" required>
-    <option selected value="">Select Time</option>
-    <option value="8:00 AM">08:00 AM</option>
-    <option value="8:30 AM">08:30 AM</option>
-    <option value="9:00 AM">09:00 AM</option>
-    <option value="9:30 AM">09:30 AM</option>
-    <option value="10:00 AM">10:00 AM</option>
-    <option value="10:30 AM">10:30 AM</option>
-    <option value="11:00 AM">11:00 AM</option>
-    <option value="11:30 AM">11:30 AM</option>
-    <option value="12:00 PM">12:00 PM</option>
-    <option value="12:30 PM">12:30 PM</option>
-    <option value="1:00 PM">01:00 PM</option>
-    <option value="1:30 PM">01:30 PM</option>
-    <option value="2:00 PM">02:00 PM</option>
-    <option value="2:30 PM">02:30 PM</option>
-    <option value="3:00 PM">03:00 PM</option>
-    </select>
+                <div class="col">
+                        <div class="card" >
+                          <img src="images/distance.jfif" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <p class="card-text">Practice physical distancing.</p>
+                          </div>
+                        </div>
+                </div>
+              </div>
+            </div>
+      </div>
+      </div>
     </div>
   </div>
   <div class="col-lg-7 col-sm-12" id="forms">
@@ -186,7 +126,7 @@ minDate: 0
         <!--INPUT FOR USER INFORMATION-->
         <div class="mb-3">
         <div class="input-group">
-        
+
         <span class="input-group-text" style="background:#1C5090; color:white;"><b>Full Name:</b></span>
         <input type="text" aria-label="First name" name="fname" class="form-control" placeholder="Juan" required>
         <input type="text" aria-label="Last name" name="lname" class="form-control" placeholder="Dela Cruz" required>
@@ -259,7 +199,7 @@ minDate: 0
         <option value="3:00 PM">03:00 PM</option>
         </select>
         </div>
-          
+
         <br>
 
         <button name = "submit" type="submit" class="btn btn-primary" style="width:100%; background:#1C5090; text-transform:uppercase; font-size:16px; font-weight:bold;">Book Appointment</button>
@@ -268,7 +208,7 @@ minDate: 0
       </div>
 
         <div class="card-footer text-muted">
-          
+
           <div class="alert alert-primary d-flex align-items-center" role="alert">
             <i class="bi bi-info-square-fill" style="margin-right:1%;"></i>
           <div>
