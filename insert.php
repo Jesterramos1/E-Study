59067de5713ccde97d40f7146b8dd05db5ade145
astherielle-08 <114635425+astherielle-08@users.noreply.github.com>
2022-11-24 12:06:16 +0800

    fixed

--- insert.php
+++ insert.php
@@ -25,7 +25,7 @@ $lName = ucwords(mysqli_real_escape_string($link, $_REQUEST['lname']));
 $studCourse = mysqli_real_escape_string($link, $_REQUEST['course']);
 $studEmail = mysqli_real_escape_string($link, $_REQUEST['email']);
 $studContact = mysqli_real_escape_string($link, $_REQUEST['contact']);
-$department = $_SESSION['selected-department'];
+$location = $_SESSION['selected-location'];
 $date_filed = date ("Y-m-d");
 $studSched = mysqli_real_escape_string($link, $_REQUEST['calendar']);
 $studTime = mysqli_real_escape_string($link, $_REQUEST['time']);
@@ -38,7 +38,7 @@ $compareTime = date('h:i');
  
 // Attempt insert query execution 
   if (isset($_POST['submit'])) {
-    $sql = "INSERT INTO `booked_schedule`(`id`, `date_filed`, `studNum`, `fName`, `lName`, `studCourse`, `studEmail`, `studContact`, `department`, `studSched`, `studTime`) VALUES ('', '$date_filed','$studNum', '$fName', '$lName', '$studCourse', '$studEmail', '$studContact', '$department', '$studSched','$studTime')";
+    $sql = "INSERT IGNORE INTO `booked_schedule`(`date_filed`, `studNum`, `fName`, `lName`, `studCourse`, `studEmail`, `studContact`, `location`, `studSched`, `studTime`) VALUES ('$date_filed','$studNum', '$fName', '$lName', '$studCourse', '$studEmail', '$studContact', '$location', '$studSched','$studTime')";
             if(mysqli_query($link, $sql)){
                 echo '<div class="card">';
                 echo '<h5 class="card-header">Your visitation is confirmed!</h5>';
@@ -52,7 +52,7 @@ $compareTime = date('h:i');
                       <th scope="col">Time:</th>
                       </tr>';
                 echo '<tr>';
-                echo '<td class="loc">' . $department . '</td>';
+                echo '<td class="loc">' . $location . '</td>';
                 echo '<td>' . $studSched . '</td>';
                 echo '<td>' . $studTime . '</td>';
                 echo '</tr>';
