62b6a0fbc71445cd5c48e7c516db1ca944ca563c
Jesterramos1 <114465045+Jesterramos1@users.noreply.github.com>
2022-11-27 01:21:08 +0800

    Newpaginationstyle

--- adminpanel.php
+++ adminpanel.php
@@ -38,7 +38,7 @@ $rs_result = mysqli_query($con, $p);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.3.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="simplePagination.css" />
+    <link rel="stylesheet" href="paginationStyle.css"/>
     <script src="jquery.simplePagination.js"></script>
     
     <title>RTU Admin View</title>
